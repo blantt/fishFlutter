41c0c3e25cd1b1b9b81f4e86de759db99b5316ba
blantt <38751053+blantt@users.noreply.github.com>
2024-10-18 15:46:02 +0800

    delok

--- android/app/src/main/kotlin/com/example/blantt_love_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.yourdomain.AMCFB
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/myBasic.dart
+++
@@ -1,4 +0,0 @@
-import 'package:blantt_love_test/Model/modalBasic.dart';
-import 'package:blantt_love_test/utils/router_test.dart';
-import 'package:blantt_love_test/component/blanttColor.dart';
-import 'package:blantt_love_test/component/blanttButton.dart';
